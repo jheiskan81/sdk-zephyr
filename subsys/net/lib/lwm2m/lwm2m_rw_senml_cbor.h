4f487f50f431095ca4403f0cfec98a855b649515
Veijo Pesonen <veijo.pesonen@nordicsemi.no>
2022-03-14 16:55:00 +0200

    [nrf noup] net: lwm2m: SenML CBOR composite obs
    Composite observe support for SenML CBOR.
    
    Signed-off-by: Veijo Pesonen <veijo.pesonen@nordicsemi.no>

--- subsys/net/lib/lwm2m/lwm2m_rw_senml_cbor.h
+++ subsys/net/lib/lwm2m/lwm2m_rw_senml_cbor.h
@@ -16,4 +16,8 @@ int do_read_op_senml_cbor(struct lwm2m_message *msg);
 int do_composite_read_op_senml_cbor(struct lwm2m_message *msg);
 int do_write_op_senml_cbor(struct lwm2m_message *msg);
 
+int do_composite_observe_parse_path_senml_cbor(struct lwm2m_message *msg,
+					       sys_slist_t *lwm2m_path_list,
+					       sys_slist_t *lwm2m_path_free_list);
+
 #endif /* LWM2M_RW_SENML_CBOR_H_ */
